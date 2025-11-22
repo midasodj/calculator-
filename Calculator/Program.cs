aefb7bb4b91e475ad05d979021543f81ff991aeb
midasodj <mikl04022007@gmail.com>
2025-11-22 14:43:59 +0500

    calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace  Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====Simple Calculator====");
+            Console.WriteLine("Введите первое число:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите операцию: ");
+            string op = Console.ReadLine();
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(Add(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine(Subtract(a, b));
+                    break;
+                case "*":
+                    Console.WriteLine(Multiply(a, b));
+                    break;
+                case "/":
+                    Console.WriteLine(Divide(a, b));
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
